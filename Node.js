8dcfd3adaabaa697823be59e96d68fcd37d99039
Devesh01007 <47006878+Devesh01007@users.noreply.github.com>
2024-06-08 17:40:23 +0530

    Update Node.js
    app.js

--- Node.js
+++ Node.js
@@ -4,3 +4,18 @@ mkdir hello-world-nodejs
 cd hello-world-nodejs
 npm init -y
 npm install express
+
+
+Create  app.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
